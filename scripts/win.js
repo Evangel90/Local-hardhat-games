cc2b6d238017a2372d36d2a72b29f6b5b2514129
Evangel90 <oladipoevangel90@gmail.com>
2023-10-10 22:49:08 +0100

    game2 won

--- scripts/win.js
+++ scripts/win.js
@@ -1,13 +1,17 @@
 // add the game address here and update the contract name if necessary
-const gameAddr = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
-const contractName = "Game1";
+const gameAddr = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
+const contractName = "Game2";
 
 async function main() {
     // attach to the game
     const game = await hre.ethers.getContractAt(contractName, gameAddr);
 
     // do whatever you need to do to win the game here:
-    const tx = await game.win();
+    const tx1 = await game.setX(30);
+    // await tx1.wait(); // might be needful in real life blockchain due to delay in processing transactions
+    const tx2 = await game.setY(20);
+    // await tx2.wait();
+    const tx3 = await game.win();
 
     // did you win? Check the transaction receipt!
     // if you did, it will be in both the logs and events array
